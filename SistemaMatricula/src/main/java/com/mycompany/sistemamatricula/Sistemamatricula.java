12b6882eab29308b83549dd85392a063b650da17
enriquemacias21 <enriquemacias21@hotmail.com>
2024-09-16 17:09:25 -0500

    Add files via upload

---
+++ SistemaMatricula/src/main/java/com/mycompany/sistemamatricula/Sistemamatricula.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemamatricula;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Sistemamatricula {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+}
